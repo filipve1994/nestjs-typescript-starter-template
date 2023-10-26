c4cb57979f826157bd15a4826f11283fad5a44b2
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2023-10-26 10:37:23 +0100

    add server port env var and default port when missing

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
 
   // https://docs.nestjs.com/techniques/validation#auto-validation
   // https://docs.nestjs.com/techniques/validation#stripping-properties
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  const port = configService.get('SERVER_PORT') || 3000;
+  await app.listen(port);
 }
 bootstrap();
