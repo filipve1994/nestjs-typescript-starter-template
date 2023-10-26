ebc82f1ea2c75285da5971edfd978b5efd4acdd6
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2023-10-26 10:42:41 +0100

    apply compression and cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+
+  // https://docs.nestjs.com/techniques/cookies
+  app.use(cookieParser());
+
+  // https://docs.nestjs.com/techniques/compression
+  app.use(compression());
 
   // https://docs.nestjs.com/techniques/validation#auto-validation
   // https://docs.nestjs.com/techniques/validation#stripping-properties
