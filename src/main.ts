0e4e956021b28aa4111cb4a9d7ec0a6fed7b96ba
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2023-10-26 10:28:28 +0100

    add  first validation packages and setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // https://docs.nestjs.com/techniques/validation#auto-validation
+  // https://docs.nestjs.com/techniques/validation#stripping-properties
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
