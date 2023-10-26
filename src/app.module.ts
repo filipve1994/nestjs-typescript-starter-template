6594023f340d3abf76f7830048c00f1041d60249
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2023-10-26 10:44:58 +0100

    add axios package

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     //https://docs.nestjs.com/techniques/configuration
     ConfigModule.forRoot(),
+    // https://docs.nestjs.com/techniques/http-module
+    HttpModule,
   ],
   controllers: [AppController],
   providers: [AppService],
